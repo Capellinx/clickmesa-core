743c840fe4c670989324d2299822b97ff38b0707
capellinx <capellaaa7@gmail.com>
2025-02-02 19:48:15 -0300

    feat: create new route to list all restaurants

--- src/modules/restaurant/external/controllers/restaurant.controller.ts
+++ src/modules/restaurant/external/controllers/restaurant.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateRestaurantDTO } from '../dtos/create-restaurant.dto';
 import { CreateRestaurantUseCase } from '../../internal/use-cases/create-restaurant.use-case';
+import { ListAllRestaurantUseCase } from '../../internal/use-cases/list-all-restaurant.use-case';
 
 @Controller('restaurant')
 export class RestaurantController {
   constructor(
     private readonly createRestaurantUseCase: CreateRestaurantUseCase,
+    private readonly listAllRestaurantUseCase: ListAllRestaurantUseCase,
   ) {}
 
   @Post()
   create(@Body() createRestaurantDTO: CreateRestaurantDTO) {
     return this.createRestaurantUseCase.execute(createRestaurantDTO);
+  }
+
+  @Get()
+  findAll() {
+    return this.listAllRestaurantUseCase.execute();
   }
 }
