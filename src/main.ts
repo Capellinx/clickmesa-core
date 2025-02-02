06f3791a6a7b542b46646aa930c0b734f9dc47b9
capellinx <capellaaa7@gmail.com>
2025-02-02 17:22:52 -0300

    chore: configure swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigModule } from '@nestjs/config';
 import { EnvService } from './env/env.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.enableCors()
-  app.use(helmet())
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
+
+  app.enableCors();
+  app.use(helmet());
+
+  const documentationConfig = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, documentationConfig);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
