f0c1e00913729b8438166af0234b5acfa501d8c3
capellinx <capellaaa7@gmail.com>
2025-02-02 14:28:29 -0300

    feat: confiure env service

---
+++ src/env/env.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const envSchema = z.object({
+   PORT: z.coerce.string().default("3333"),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+   console.error('⚠️ Invalid enviroment variables!', _env.error.format())
+
+   throw new Error('Invalid enviroment variables!')
+}
+
+export type Env = z.infer<typeof envSchema>
