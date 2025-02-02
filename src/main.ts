3cac9306e83b08b9cd2c229c43c248098ed0527e
capellinx <capellaaa7@gmail.com>
2025-02-02 14:38:20 -0300

    chore: configure helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigModule } from '@nestjs/config';
 import { EnvService } from './env/env.service';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors()
+  app.use(helmet())
 
   const configService = app.get(EnvService);
   const port = configService.get('PORT');
