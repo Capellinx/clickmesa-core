d5465017753e1ab40e70e7e8a9d5e9b4009c7792
capellinx <capellaaa7@gmail.com>
2025-02-02 19:48:24 -0300

    feat: create new contract to list

--- src/modules/restaurant/domain/repositories/restaurant.repository.ts
+++ src/modules/restaurant/domain/repositories/restaurant.repository.ts
@@ -1,6 +1,8 @@
 import { CreateRestaurantDTO } from '../../external/dtos/create-restaurant.dto';
+import { ListAllRestaurantDTO } from '../../external/dtos/list-all-restaurant.dto';
 
 export interface IRestaurantRepository {
   create(props: CreateRestaurantDTO): Promise<void>;
   findByEmail(email: string): Promise<boolean>;
+  findAll(): Promise<ListAllRestaurantDTO[]>;
 }
