a599045fd1fe02cf3121c05ad71ceb1f4c830159
capellinx <capellaaa7@gmail.com>
2025-02-02 17:22:40 -0300

    refactor: remove validation zod

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,15 +1,8 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
-  PORT: z.coerce.string().default('3333'),
+  PORT: z.string().describe('Porta do servidor'),
 });
 
-const _env = envSchema.safeParse(process.env);
-
-if (_env.success === false) {
-  console.error('⚠️ Invalid enviroment variables!', _env.error.format());
-
-  throw new Error('Invalid enviroment variables!');
-}
 
 export type Env = z.infer<typeof envSchema>;
