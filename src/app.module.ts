2eee447fe712f9b58b36500c51f82ef011d8d0a7
capellinx <capellaaa7@gmail.com>
2025-02-03 02:34:03 -0300

    feat: create auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -36,4 +36,4 @@ import { ServicesModule } from './modules/services/services.module';
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {}
