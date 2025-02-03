22ee017a4abe62cde5257a07260cbd8d85adeb9c
capellinx <capellaaa7@gmail.com>
2025-02-02 22:45:20 -0300

    feat: configure nodemailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,28 @@ import { EnvModule } from './env/env.module';
 import { ConfigModule } from '@nestjs/config';
 import { envSchema } from './env/env';
 import { RestaurantModule } from './modules/restaurant/restaurant.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
+    }),
+    MailerModule.forRootAsync({
+      useFactory: async () => {
+        const env = envSchema.parse(process.env);
+        return {
+          transport: {
+            host: env.HOST_MAIL,
+            port: env.PORT_MAIL,
+            auth: {
+              user: env.USER_MAIL,
+              pass: env.PASSWORD_MAIL,
+            },
+          },
+        };
+      },
     }),
     EnvModule,
     RestaurantModule,
