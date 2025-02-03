1f53042da1eb67bcfdaccceebf8a62ab2d42ddc5
capellinx <capellaaa7@gmail.com>
2025-02-02 22:45:39 -0300

    feat: typing envs

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,6 +2,10 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   PORT: z.string().describe('Porta do servidor'),
+  HOST_MAIL: z.string().describe('Host do provider de email'),
+  PORT_MAIL: z.coerce.number().describe('Porta do provider de email'),
+  USER_MAIL: z.string().describe('Usuário do provider de email'),
+  PASSWORD_MAIL: z.string().describe('Senha do provider de email'),
 });
 
 export type Env = z.infer<typeof envSchema>;
